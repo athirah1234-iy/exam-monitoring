bbedbb0fada67b644ddb676b329c8a1f1b7119e5
Athirah1234-iy <2022835854@student.uitm.edu.my>
2025-06-16 02:42:45 +0800

    Switch to Docker for Python 3.9

--- server.py
+++ server.py
@@ -19,5 +19,9 @@ def handle_upload():
     blob.upload_from_file(video)
     return "Upload successful! Analysis will begin shortly."
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
